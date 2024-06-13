fcd971201911a51b62bb2f487fdaf2f1721658b7
Anatolii1203 <v120386t@gmail.com>
2024-06-13 18:13:19 +0300

    js-11

---
+++ src/js/render-functions.js
@@ -0,0 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+let lightbox;
+
+export function createImg(image) {
+  return `<li class="card">
+    <a href="${image.largeImageURL}">
+        <img src="${image.webformatURL}" alt="${image.tags}">
+    </a>
+    <div class="card-info">
+        <p class = "description"></b>Likes: ${image.likes}</p>
+        <p class = "description"></b>Views: ${image.views}</p>
+        <p class = "description"></b>Comments: ${image.comments}</p>
+        <p class = "description"></b>Downloads: ${image.downloads}</p>
+    </div>
+</li>`;
+}
+
+export function downLoadImg(arr) {
+  return arr.map(createImg).join('');
+}
+
+export function imgGallery() {
+  if (!lightbox) {
+    lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+  } else {
+    lightbox.refresh();
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,57 @@
+import { findImg } from './js/pixabay-api';
+import { downLoadImg } from './js/render-functions';
+import { createImg } from './js/render-functions';
+import { imgGallery } from './js/render-functions';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const from = document.querySelector('.form');
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('#loading');
+
+from.addEventListener('submit', event => {
+  event.preventDefault();
+  gallery.innerHTML = '';
+
+  const request = event.target.request.value.trim();
+
+  findImg(request)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        return;
+      } else {
+        const markup = downLoadImg(data.hits);
+        gallery.innerHTML = markup;
+        showLoading();
+        imgGallery();
+      }
+    })
+    .catch(error => {
+      iziToast.error({
+        message: `An error occurred: ${error.message}. Please try again later.`,
+        position: 'topRight',
+      });
+    })
+    .finally(() => {
+      event.target.reset();
+      hideLoading();
+    });
+});
+
+function showLoading() {
+  loader.style.display = 'block';
+}
+
+function hideLoading() {
+  loader.style.display = 'none';
+}
+
+// window.addEventListener('load', () => {
+//   hideLoading();
+// });
